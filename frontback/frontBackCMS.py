936648eced7c17bd5c6eeb4df1d22810a1a72d56
SB - Number <spicer.bak@usace.army.mil>
2019-03-26 12:21:54 +0000

    updated cms to continue plotting/processing savepoint data if get data returns no data (or errors)

--- frontback/frontBackCMS.py
+++ frontback/frontBackCMS.py
@@ -366,124 +366,123 @@ def CMSanalyze(startTime, inputDict):
     stationList = ['waverider-26m', 'waverider-17m', 'awac-11m', '8m-array', 'awac-6m', 'awac-4.5m', 'adop-3.5m',
                    'xp200m', 'xp150m', 'xp125m']
     for gg, station in enumerate(stationList):
-        # stationName = 'CMTB-waveModels_CMS_%s_%s' % (version_prefix, station)  # xp 125
 
-        # this needs to be the same order as the run script
-        stat_yaml_fname = 'yaml_files/waveModels/{}/Station_var.yml'.format(fldrArch)
-        globalyaml_fname = 'yaml_files/waveModels/{}/Station_globalmeta.yml'.format(fldrArch)
-        # getting lat lon, easting northing idx's
-        # Idx_i = len(gridPack['i']) - np.argwhere(gridPack['i'] == stat_packet['iStation'][
-        #     gg]).squeeze() - 1  # to invert the coordinates from offshore 0 to onshore 0
-        # Idx_j = np.argwhere(gridPack['j'] == stat_packet['jStation'][gg]).squeeze()
-        if pFlag == True:
-            w = go.getWaveSpec(station)  # go get all data
-        else:
-            w = go.getWaveGaugeLoc(station)
-        # print '   Comparison location taken from thredds, check positioning '
-        stat_data = {'time': nc.date2num(stat_packet['time'][:], units='seconds since 1970-01-01 00:00:00'),
-                     'waveHs': stat_packet['waveHs'][:, gg],
-                     'waveTm': np.ones_like(stat_packet['waveHs'][:, gg]) * -999,
-                     # this isn't output by model, but put in fills to stay consitant
-                     'waveDm': stat_packet['WaveDm'][:, gg],
-                     'waveTp': stat_packet['Tp'][:, gg],
-                     'waterLevel': stat_packet['waterLevel'][:, gg],
-                     'swellHs': stat_packet['swellHs'][:, gg],
-                     'swellTp': stat_packet['swellTp'][:, gg],
-                     'swellDm': stat_packet['swellDm'][:, gg],
-                     'seaHs': stat_packet['seaHs'][:, gg],
-                     'seaTp': stat_packet['seaTp'][:, gg],
-                     'seaDm': stat_packet['seaDm'][:, gg],
-                     'station_name': station,
-                     'directionalWaveEnergyDensity': obse_packet['ncSpec'][:, gg, :, :],
-                     'waveDirectionBins': obse_packet['ncDirs'],
-                     'waveFrequency': obse_packet['wavefreqbin'],
-                     ###############################
-                     'DX': dep_pack['dx'],
-                     'DY': dep_pack['dy'],
-                     'NI': dep_pack['NI'],
-                     'NJ': dep_pack['NJ'],
-                     'grid_azimuth': gridPack['azimuth']}
         try:
-            stat_data['Latitude'] = w['latitude']
-            stat_data['Longitude'] = w['longitude']
-        except KeyError:  # this should be rectified
-            stat_data['Latitude'] = w['lat']
-            stat_data['Longitude'] = w['lon']
-        # Name files and make sure server directory has place for files to go
-        print 'making netCDF for model output at %s ' % station
-        TdsFldrBase = os.path.join(Thredds_Base, fldrArch, station)
+            # generate yaml file name
+            stat_yaml_fname = 'yaml_files/waveModels/{}/Station_var.yml'.format(fldrArch)
+            globalyaml_fname = 'yaml_files/waveModels/{}/Station_globalmeta.yml'.format(fldrArch)
+            # go get data or locations depending on if we're plotting against data
+            if pFlag == True:
+                w = go.getWaveSpec(station)  # go get all data
+            else:
+                w = go.getWaveGaugeLoc(station)
+            # print '   Comparison location taken from thredds, check positioning '
+            stat_data = {'time': nc.date2num(stat_packet['time'][:], units='seconds since 1970-01-01 00:00:00'),
+                         'waveHs': stat_packet['waveHs'][:, gg],
+                         'waveTm': np.ones_like(stat_packet['waveHs'][:, gg]) * -999, # this isn't output by model, but put in fills to stay constant
+                         'waveDm': stat_packet['WaveDm'][:, gg],
+                         'waveTp': stat_packet['Tp'][:, gg],
+                         'waterLevel': stat_packet['waterLevel'][:, gg],
+                         'swellHs': stat_packet['swellHs'][:, gg],
+                         'swellTp': stat_packet['swellTp'][:, gg],
+                         'swellDm': stat_packet['swellDm'][:, gg],
+                         'seaHs': stat_packet['seaHs'][:, gg],
+                         'seaTp': stat_packet['seaTp'][:, gg],
+                         'seaDm': stat_packet['seaDm'][:, gg],
+                         'station_name': station,
+                         'directionalWaveEnergyDensity': obse_packet['ncSpec'][:, gg, :, :],
+                         'waveDirectionBins': obse_packet['ncDirs'],
+                         'waveFrequency': obse_packet['wavefreqbin'],
+                         ###############################
+                         'DX': dep_pack['dx'],
+                         'DY': dep_pack['dy'],
+                         'NI': dep_pack['NI'],
+                         'NJ': dep_pack['NJ'],
+                         'grid_azimuth': gridPack['azimuth']}
+            try:
+                stat_data['Latitude'] = w['latitude']
+                stat_data['Longitude'] = w['longitude']
+            except KeyError:  # this should be rectified
+                stat_data['Latitude'] = w['lat']
+                stat_data['Longitude'] = w['lon']
+            # Name files and make sure server directory has place for files to go
+            print 'making netCDF for model output at %s ' % station
+            TdsFldrBase = os.path.join(Thredds_Base, fldrArch, station)
 
-        NCpath = sb.makeNCdir(Thredds_Base, os.path.join(version_prefix, station), datestring, model='CMS')
-        # make the name of this nc file
-        NCname = 'CMTB-waveModels_{}_{}_{}_{}.nc'.format(model, version_prefix, station, datestring)
-        outFileName = os.path.join(NCpath, NCname)
+            NCpath = sb.makeNCdir(Thredds_Base, os.path.join(version_prefix, station), datestring, model='CMS')
+            # make the name of this nc file
+            NCname = 'CMTB-waveModels_{}_{}_{}_{}.nc'.format(model, version_prefix, station, datestring)
+            outFileName = os.path.join(NCpath, NCname)
 
-        if not os.path.exists(TdsFldrBase):
-            os.makedirs(TdsFldrBase)  # make the directory for the file/ncml to go into
-        if not os.path.exists(os.path.join(TdsFldrBase, station + '.ncml')):
-            inputOutput.makencml(os.path.join(TdsFldrBase, station + '.ncml'))
-        # make netCDF
-        makenc.makenc_Station(stat_data, globalyaml_fname=globalyaml_fname, flagfname=flagfname,
-                              ofname=outFileName, stat_yaml_fname=stat_yaml_fname)
+            if not os.path.exists(TdsFldrBase):
+                os.makedirs(TdsFldrBase)  # make the directory for the file/ncml to go into
+            if not os.path.exists(os.path.join(TdsFldrBase, station + '.ncml')):
+                inputOutput.makencml(os.path.join(TdsFldrBase, station + '.ncml'))
+            # make netCDF
+            makenc.makenc_Station(stat_data, globalyaml_fname=globalyaml_fname, flagfname=flagfname,
+                                  ofname=outFileName, stat_yaml_fname=stat_yaml_fname)
 
-        print "netCDF file's created for station: %s " % station
-        ###################################################################################################################
-        ###############################   Plotting  Below   ###############################################################
-        ###################################################################################################################
+            print "netCDF file's created for station: %s " % station
+            ###################################################################################################################
+            ###############################   Plotting  Below   ###############################################################
+            ###################################################################################################################
 
-        if pFlag == True and 'time' in w:
-            if full == False:
-                w['dWED'], w['wavedirbin'] = prepdata.HPchop_spec(w['dWED'], w['wavedirbin'], angadj=70)
-            obsStats = sbwave.waveStat(w['dWED'], w['wavefreqbin'], w['wavedirbin'])
+            if pFlag == True and 'time' in w:
+                if full == False:
+                    w['dWED'], w['wavedirbin'] = prepdata.HPchop_spec(w['dWED'], w['wavedirbin'], angadj=70)
+                obsStats = sbwave.waveStat(w['dWED'], w['wavefreqbin'], w['wavedirbin'])
 
-            modStats = sbwave.waveStat(obse_packet['ncSpec'][:, gg, :, :], obse_packet['wavefreqbin'],
-                                       obse_packet['ncDirs'])  # compute model stats here
+                modStats = sbwave.waveStat(obse_packet['ncSpec'][:, gg, :, :], obse_packet['wavefreqbin'],
+                                           obse_packet['ncDirs'])  # compute model stats here
 
-            time, obsi, modi = sb.timeMatch(nc.date2num(w['time'], 'seconds since 1970-01-01'),
-                                            np.arange(w['time'].shape[0]),
-                                            nc.date2num(stat_packet['time'][:], 'seconds since 1970-01-01'),
-                                            np.arange(len(stat_packet['time'])))  # time match
+                time, obsi, modi = sb.timeMatch(nc.date2num(w['time'], 'seconds since 1970-01-01'),
+                                                np.arange(w['time'].shape[0]),
+                                                nc.date2num(stat_packet['time'][:], 'seconds since 1970-01-01'),
+                                                np.arange(len(stat_packet['time'])))  # time match
 
-            for param in modStats:  # loop through each bulk statistic
-                if len(time) > 1 and param in ['Hm0', 'Tm', 'sprdF', 'sprdD', 'Tp', 'Dm']:
-                    print '    plotting %s: %s' % (station, param)
-                    if param in ['Tp', 'Tm10']:
-                        units = 's'
-                        title = '%s period' % param
-                    elif param in ['Hm0']:
-                        units = 'm'
-                        title = 'Wave Height %s ' % param
-                    elif param in ['Dm', 'Dp']:
-                        units = 'degrees'
-                        title = 'Direction %s' % param
-                    elif param in ['sprdF', 'sprdD']:
-                        units = '_.'
-                        title = 'Spread %s ' % param
+                for param in modStats:  # loop through each bulk statistic
+                    if len(time) > 1 and param in ['Hm0', 'Tm', 'sprdF', 'sprdD', 'Tp', 'Dm']:
+                        print '    plotting %s: %s' % (station, param)
+                        if param in ['Tp', 'Tm10']:
+                            units = 's'
+                            title = '%s period' % param
+                        elif param in ['Hm0']:
+                            units = 'm'
+                            title = 'Wave Height %s ' % param
+                        elif param in ['Dm', 'Dp']:
+                            units = 'degrees'
+                            title = 'Direction %s' % param
+                        elif param in ['sprdF', 'sprdD']:
+                            units = '_.'
+                            title = 'Spread %s ' % param
 
-                    # now run plots
-                    p_dict = {'time': nc.num2date(time, 'seconds since 1970-01-01'),
-                              'obs': obsStats[param][obsi.astype(int)],
-                              'model': modStats[param][modi.astype(int)],
-                              'var_name': param,
-                              'units': units,  # ) -> this will be put inside a tex math environment!!!!
-                              'p_title': title}
+                        # now run plots
+                        p_dict = {'time': nc.num2date(time, 'seconds since 1970-01-01'),
+                                  'obs': obsStats[param][obsi.astype(int)],
+                                  'model': modStats[param][modi.astype(int)],
+                                  'var_name': param,
+                                  'units': units,  # ) -> this will be put inside a tex math environment!!!!
+                                  'p_title': title}
 
-                    ofname = os.path.join(fpath, 'figures/Station_%s_%s_%s.png' % (station, param, datestring))
-                    stats = obs_V_mod_TS(ofname, p_dict, logo_path='ArchiveFolder/CHL_logo.png')
+                        ofname = os.path.join(fpath, 'figures/Station_%s_%s_%s.png' % (station, param, datestring))
+                        stats = obs_V_mod_TS(ofname, p_dict, logo_path='ArchiveFolder/CHL_logo.png')
 
-                    if station == 'waverider-26m' and param == 'Hm0':
-                        # this is a fail safe to abort run if the boundary conditions don't
-                        # meet quality standards below
-                        bias = 0.1  # bias has to be within 10 centimeters
-                        RMSE = 0.1  # RMSE has to be within 10 centimeters
-                        if isinstance(p_dict['obs'], np.ma.masked_array) and ~p_dict['obs'].mask.any():
-                            p_dict['obs'] = np.array(p_dict['obs'])
-                        # try:
-                        #     # assert stats['RMSE'] < RMSE, 'RMSE test on spectral boundary energy failed'
-                        #     # assert np.abs(stats['bias']) < bias, 'bias test on spectral boundary energy failed'
-                        # except:
-                        #     print '!!!!!!!!!!FAILED BOUNDARY!!!!!!!!'
-                        #     print 'deleting data from thredds!'
-                        #     os.remove(fieldOfname)
-                        #     os.remove(outFileName)
-                        #     raise RuntimeError('The Model Is not validating its offshore boundary condition')
+                        if station == 'waverider-26m' and param == 'Hm0':
+                            # this is a fail safe to abort run if the boundary conditions don't
+                            # meet quality standards below
+                            bias = 0.1  # bias has to be within 10 centimeters
+                            RMSE = 0.1  # RMSE has to be within 10 centimeters
+                            if isinstance(p_dict['obs'], np.ma.masked_array) and ~p_dict['obs'].mask.any():
+                                p_dict['obs'] = np.array(p_dict['obs'])
+                            # try:
+                            #     # assert stats['RMSE'] < RMSE, 'RMSE test on spectral boundary energy failed'
+                            #     # assert np.abs(stats['bias']) < bias, 'bias test on spectral boundary energy failed'
+                            # except:
+                            #     print '!!!!!!!!!!FAILED BOUNDARY!!!!!!!!'
+                            #     print 'deleting data from thredds!'
+                            #     os.remove(fieldOfname)
+                            #     os.remove(outFileName)
+                            #     raise RuntimeError('The Model Is not validating its offshore boundary condition')
+        except IndexError:
+            # if an index error is raised (from get data, returns no data), keep processing the rest of the stations
+            continue
